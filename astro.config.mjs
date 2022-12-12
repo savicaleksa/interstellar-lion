83f17298aa94134bb85e887b99ceff0861c4246c
savicaleksa <aleksa.savic91@gmail.com>
2022-12-12 18:47:25 +0100

    init

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
+  output: "server",
+  adapter: vercel(),
+});
