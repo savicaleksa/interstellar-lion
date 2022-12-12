83f8a8ddefb848a96bad7eb37bd7267bebff315e
savicaleksa <aleksa.savic91@gmail.com>
2022-12-12 20:47:25 +0100

    test squoosh

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,12 +11,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [react(), image()],
   output: "server",
   adapter: vercel(),
 });
